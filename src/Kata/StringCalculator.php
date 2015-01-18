69ad073a32edb86d48c0fbe250ddacd54a46cea0
Dariusz Wrzesien <dariuszwrzesien@gmail.com>
2015-01-18 23:55:58 +0100

    upgraded stringCalculator

--- src/Kata/StringCalculator.php
+++ src/Kata/StringCalculator.php
@@ -3,5 +3,21 @@ namespace Kata;
 
 class StringCalculator
 {
-
+    public function add($a = null, $b = null)
+    {
+        
+        if ($a === null && $b === null) {
+            return 0;
+        }
+        if ($a !== null && $b !== null) {
+            return $a + $b;
+        }
+        if ($a !== null && $b === null) {
+            return $a;
+        }
+        if ($a === null && $b !== null) {
+            return $b;
+        }
+        
+    }
 }
--- src/Kata/Tests/StringCalculatorTest.php
+++ src/Kata/Tests/StringCalculatorTest.php
@@ -4,10 +4,65 @@ namespace Kata;
 class StringCalculatorTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @test
+     * @var StringCalculator 
      */
-    public function first()
+    private $stringCalculator;
+    
+    public function setUp() {
+        parent::setUp();
+        $this->stringCalculator = new StringCalculator();
+    }
+    
+    public function testAdderWithoutValue()
     {
-        $this->assertTrue(true);
+        $this->assertSame(0, $this->stringCalculator->add());
+    }
+    
+    /**
+     * @dataProvider oneValueProvider
+     * @param int $a
+     * @param int $expected
+     */
+    public function testAdderWithObeValue($a, $expected)
+    {
+        $this->assertSame($expected, $this->stringCalculator->add($a));
+    }
+    
+    /**
+     * @dataProvider twoValueProvider
+     * @param type $a
+     * @param type $b
+     * @param type $expected
+     */
+    public function testAdderWithTwoValues($a, $b, $expected)
+    {
+        $this->assertSame($expected, $this->stringCalculator->add($a, $b));
+    }
+    
+    /**
+     * @return array
+     */
+    public function oneValueProvider()
+    {
+        return array(
+          array(0,0),  
+          array(1,1),  
+          array(9,9)
+        );
+    }
+    
+    /**
+     * @return array
+     */
+    public function twoValueProvider()
+    {
+        return array(
+          array(0,0,0),  
+          array(1,1,2),  
+          array(0,1,1),
+          array(1,2,3),
+          array(1,0,1),
+          array(2,1,3)
+        );
     }
 }
