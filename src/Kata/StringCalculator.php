6b8325cdefa85a5ec3fc1aeb347eb50380bcf417
Dariusz Wrzesien <dariuszwrzesien@gmail.com>
2015-01-21 06:58:54 +0000

    added first art of stringCalculator Kata

--- src/Kata/StringCalculator.php
+++ src/Kata/StringCalculator.php
@@ -1,7 +1,59 @@
 <?php
+
 namespace Kata;
 
 class StringCalculator
 {
-
+    /**
+     * @param string $string
+     * @return string
+     */
+    public function add($string)
+    {
+        if('' === $string){
+            return 0;
+        }
+        
+        $result = 0;
+        $values = $this->extract($string);
+        foreach($values as $val) {
+            $result += $val;
+        }
+        
+        return (string)$result;
+    }
+    
+    /**
+     * @param string $string
+     * @return array
+     */
+    private function extract($string)
+    {
+        if($this->isDelimiter($string)){
+            $delimiter = $this->getDelimiter($string);
+            $pattern = sprintf('/(\\n|,|%s)/', $delimiter);
+        } else {
+            $pattern = sprintf('/(\\n|,)/');
+        }
+        
+        return preg_split($pattern, $string);
+    }
+    
+    /**
+     * @param string $string
+     * @return boolean
+     */
+    private function isDelimiter($string)
+    {
+        if(mb_substr($string, 0, 2) === '//'){
+            return true;
+        }
+        
+        return false;
+    }
+    
+    private function getDelimiter($string)
+    {
+        return substr(substr($string, 2), 0, strpos(substr($string, 2), "\n"));
+    }
 }
--- src/Kata/Tests/StringCalculatorTest.php
+++ src/Kata/Tests/StringCalculatorTest.php
@@ -1,13 +1,135 @@
 <?php
+
 namespace Kata;
 
 class StringCalculatorTest extends \PHPUnit_Framework_TestCase
 {
     /**
+     * @var StringCalculator
+     */
+    private $stringCalculator;
+    
+    public function setUp() {
+        parent::setUp();
+        $this->stringCalculator = new StringCalculator();
+    }
+    
+    /**
      * @test
      */
-    public function first()
+    public function addingEmptyString()
     {
-        $this->assertTrue(true);
+        $this->assertSame(0, $this->stringCalculator->add(''));
+    }
+    
+    /**
+     * @test
+     * @dataProvider oneValueProvider
+     * @param string $string
+     * @param string $expected
+     */
+    public function addingOneValue($string, $expected)
+    {
+        $this->assertSame($expected, $this->stringCalculator->add($string));
+    }
+    
+    /**
+     * @return array
+     */
+    public function oneValueProvider()
+    {
+        return array(
+            array('0', '0'),
+            array('1', '1'),
+            array('2', '2')
+        );
+    }
+    
+    /**
+     * @test
+     * @dataProvider twoValueProvider
+     * @param string $values
+     * @param string $expected
+     */
+    public function addingTwoValues($values, $expected)
+    {
+        $this->assertSame($expected, $this->stringCalculator->add($values));
+    }
+    
+    /**
+     * @return array
+     */
+    public function twoValueProvider()
+    {
+        return array(
+            array('0, 1', '1'),
+            array('1, 1', '2'),
+            array('2, 0', '2')
+        );
+    }
+    
+    /**
+     * @test
+     * @dataProvider multipleValuesProvider
+     * @param string $values
+     * @param string $expected
+     */
+    public function addingMultipleValues($values, $expected)
+    {
+        $this->assertSame($expected, $this->stringCalculator->add($values));
+    }
+    
+    public function multipleValuesProvider()
+    {
+        return array(
+            array('1,2,3', '6'),
+            array('0,1,2,4', '7'),
+            array('0,2,0,1,0', '3'),
+        );
+    }
+    
+    /**
+     * @test
+     * @dataProvider multipleValuesWithNewLineProvider
+     * @param string $values
+     * @param string $expected
+     */
+    public function addingMultipleValuesWithNewLineDelimiter($values, $expected)
+    {
+        $this->assertSame($expected, $this->stringCalculator->add($values));
+    }
+    
+    /**
+     * @return array
+     */
+    public function multipleValuesWithNewLineProvider()
+    {
+        return array(
+            array("1\n2,3", '6'),
+            array("1,2\n3", '6'),
+            array("0\n1\n1", '2')
+        );
+    }
+    
+    /**
+     * @test
+     * @dataProvider multipleValuesWithDifferentDelimetersProvider
+     * @param string $values
+     * @param string $expected
+     */
+    public function addingMultipleValuesWithDifferentDelimetersDelimiter($values, $expected)
+    {
+        $this->assertSame($expected, $this->stringCalculator->add($values));
+    }
+    
+    /**
+     * @return array
+     */
+    public function multipleValuesWithDifferentDelimetersProvider()
+    {
+        return array(
+            array("//;\n1;2;3", '6'),
+            array("//dupa\n1dupa2dupa3", '6'),
+        );
     }
 }
