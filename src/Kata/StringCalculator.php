fbf4079b5fce5d98efb745f40e9c6e269b2d4d3b
Dariusz Wrzesien <dariuszwrzesien@gmail.com>
2015-01-21 21:59:00 +0100

    stringCalulator

--- src/Kata/StringCalculator.php
+++ src/Kata/StringCalculator.php
@@ -2,7 +2,85 @@
 
 namespace Kata;
 
+use Exception;
+
 class StringCalculator
 {
+    /**
+     * @param string $string
+     * @return int
+     */
+    public function add($string)
+    {
+        if ('' === $string) {
+           return 0; 
+        }
+        
+        $result = 0;
+        $values = $this->getValues($string);
+        
+        foreach ($values as $val) {          
+            $result += $val;
+        }
+        
+        return (int)$result;
+        
+    }
     
+    /**
+     * @param string $string
+     * @return array
+     */
+    private function getValues($string)
+    {
+        $delimiters = array(',', '\n');
+        
+        if ($this->isDifferentDelimiter($string)) {
+            $delimiters[] = $this->getDelimiter($string);
+        }
+        
+        $values = preg_split(sprintf("/(%s)/", $delimiters), $string);
+        
+        return $this->validate($values);
+    }
+    
+    /**
+     * @param string $string
+     * @return boolean
+     */
+    private function isDifferentDelimiter($string)
+    {
+        return ('//' === mb_substr($string, 0, 2));
+    }
+    
+    /**
+     * @param string $string
+     * @return string
+     */
+    private function getDelimiter($string)
+    {
+        return mb_substr($string, 2, mb_strpos(mb_substr($string, 2), "\n"));
+    }
+    
+    /**
+     * @param array $values
+     * @return array
+     * @throws Exception
+     */
+    private function validate($values)
+    {
+        $errValues = array();
+        
+        foreach ($values as $val) {
+            if($val < 0){
+                $errValues[] = $val;
+            }
+        }
+        
+        if ( ! empty($errValues)) {
+            throw new Exception(sprintf("negatives not allowed %s", implode(',', $errValues)));
+        }
+        
+        return $values;
+    }
 }
--- src/Kata/Tests/StringCalculatorTest.php
+++ src/Kata/Tests/StringCalculatorTest.php
@@ -6,9 +6,39 @@ class StringCalculatorTest extends \PHPUnit_Framework_TestCase
 {
     /**
      * @test
+     * @dataProvider valuesProvider
+     * @param string $values
+     * @param int $expected
      */
-    public function first()
+    public function addEmptyStringValue($values, $expected)
     {
-        $this->assertTrue(true);
+        $stringCalculator = new StringCalculator();
+        $this->assertSame($expected, $stringCalculator->add($values));
+    }
+    
+    public function valuesProvider(){
+        return array(
+            array('', 0),
+            array('0', 0),
+            array('1', 1),
+            array('1,1', 2),
+            array('1,2,3', 6),
+            array('1,0,0,1', 2),
+            array("1\n2,3", 6),
+            array("1\n2\n3,0,1\n0,1", 8),
+            array("1,\n", 1),
+            array("//;\n1;2;1", 4),
+        );
+    }
+    
+    /**
+     * @test
+     * @expectedException Exception
+     * @expectedExceptionMessage negatives not allowed -1,-2,-1
+     */
+    public function negativesThrowException()
+    {
+        $stringCalculator = new StringCalculator();
+        $stringCalculator->add("-1,0\n-2,1,-1");
     }
 }
